478a1ee0b3612999ff4aa490bcd8fb1ff0d35f49
Leah Ferrell <leahferrell03@gmail.com>
2023-12-29 23:52:18 -0600

    cleaning things up

--- src/app/app.js
+++ src/app/app.js
@@ -25,22 +25,24 @@ const App = () => {
 
 	return (
 		<div className="container">
-			<h1>
+			{!isSuccess && <h1>
 				Please enter your three-digit PIN to continue
-			</h1>
-			<div>
+			</h1>}
+			{!isSuccess && <div>
 				<div>
 					<input min="1" max="9" type="number" name="pin1" onChange={(event) => setNumber1(event.target.value)}/>
 					<input min="1" max="9" type="number" name="pin2" onChange={(event) => setNumber2(event.target.value)}/>
 					<input min="1" max="9" type="number" name="pin3" onChange={(event) => setNumber3(event.target.value)}/>
 				</div>
 				<button onClick={onClick}>Submit</button>
-			</div>
-			{isSuccess && <div>
-				<h2>Access granted! Next, you will meet your onboarding instructor. Please connect with them to receive a special message.</h2>
 			</div>}
-			{isFailure && <div>
-				<h2>Incorrect. Please review your pre-onboarding assignments. If you have any questions, then please reach out to your companion-bot</h2>
+			{isSuccess && <div className="success">
+				<h2>Access granted!</h2>
+				<p>Next, you will meet your onboarding instructor. Please connect with them to receive a special message.</p>
+			</div>}
+			{isFailure && <div className="failure">
+				<h2>Incorrect</h2>
+				<p>Please review your pre-onboarding assignments. If you have any questions, then please reach out to your companion-bot</p>
 			</div>}
 		</div>
 	)
