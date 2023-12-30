7ddef18790bf382bf8e2b6b5d3b9c17eed71bce6
Leah Ferrell <leahferrell03@gmail.com>
2023-12-29 23:07:37 -0600

    finished basic scaffolding

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,42 @@
-import React from 'react'
-import meme1 from '../images/meme1.jpeg'
-import meme2 from '../images/meme2.jpeg'
-import meme3 from '../images/meme3.webp'
+import React, {useState} from 'react'
+
+import * as config from '../config/config'
+
 import './app.css'
 
 const App = () => {
+	const [number1, setNumber1] = useState(null)
+	const [number2, setNumber2] = useState(null)
+	const [number3, setNumber3] = useState(null)
+	const [isSuccess, setIsSuccess] = useState(false)
+	const [isFailure, setIsFailure] = useState(false)
+
+	const onClick = (event) => {
+		event.preventDefault()
+
+		if (number1 === config.correctNumber1 && number2 === config.correctNumber2 && number3 === config.correctNumber3) {
+			setIsSuccess(true)
+			setIsFailure(false)
+		} else {
+			setIsSuccess(false)
+			setIsFailure(true)
+		}
+	}
+
 	return (
 		<div>
-			<h1>You have been hacked!!</h1>
-			<p>These aren't the plans you are looking for.</p>
-			<p>Now we've hijacked <em>The Devastator!!</em> If you want to find it again you will need to complete a quest.</p>
-			<p>Here are some memes while you search for your cherished Imperial Star Destroyer.</p>
-			<h2>Memes</h2>
-			<img alt="lost the death star oops" src={meme1} />
-			<img alt="the plan is working" src={meme2} />
-			<img alt="death star pro max" src={meme3} />
-			<h2>Oh wait... You don't want memes? Sorry Lord Vader... Just trying to get with times. I can't directly help you get your Star Destroyer back, but maybe you'll figure something out if you use your Tie Fighter.</h2>
+			<input min="1" max="9" type="number" name="pin1" onChange={(event) => setNumber1(event.target.value)}/>
+			<input min="1" max="9" type="number" name="pin2" onChange={(event) => setNumber2(event.target.value)}/>
+			<input min="1" max="9" type="number" name="pin3" onChange={(event) => setNumber3(event.target.value)}/>
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			{isSuccess && <div>
+				<p>Hooray! Those are the correct numbers!</p>
+			</div>}
+			{isFailure && <div>
+				<p>Incorrect. Please review your numbers.</p>
+			</div>}
 		</div>
 	)
 }
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+export const correctNumber1 = "2"
+export const correctNumber2 = "4"
+export const correctNumber3 = "9"
